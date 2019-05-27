2cb058627d09ef77329f2dc6ceffa336e727ea64
salties <y.yan@bristol.ac.uk>
2019-05-27 19:30:53 +0100

    Update algorithm.

--- maxarea.c
+++ maxarea.c
@@ -23,22 +23,36 @@ int maxArea(int *height, int heightSize)
     int maxarea = 0;
     
     i = 0;
-    j = 1;
-    for(i = 0; i < j; i++)
+    j = heightSize - 1;
+    
+    while(i < j - 1)
     {
-        for(j = 1; j < heightSize; j++)
+        //Record the current area.
+        maxarea = max(maxarea, area(height, heightSize, i, j));
+        
+        //Move the boundary.
+        if(height[i] < height[j])
         {
-            maxarea = max(maxarea, area(height, heightSize, i, j));
+            i++;
         }
+        else
+        {
+            j--;
+        }
+        
     }
 
 	return maxarea;
 }
 
+#if 0
 int hist[] = {
 	1, 8, 6, 2, 5,
 	4, 8, 3, 7
 };
+#endif
+
+int hist[] = {1,2,4,3};
 
 int main()
 {
